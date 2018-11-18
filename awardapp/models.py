e51df1a8fd1bf5cd11300ddebd903a4f96cf9a87
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-18 17:11:23 +0300

    created first models

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+
+class Site(models.Model):
+    screenshot = models.ImageField(upload_to = 'images/', blank = True)
+    site_name = models.CharField(max_length = 30)
+    description = HTMLField(max_length = 70)
