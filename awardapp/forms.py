3d00a2289e1caefc0fc52f6cbe48f15e5c962bab
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-19 15:51:55 +0300

    search

--- awardapp/forms.py
+++ awardapp/forms.py
@@ -1,1 +1,12 @@
+rom django import forms
+from .models import Site,Profile
 
+class SiteForm(forms.ModelForm):
+    class Meta:
+        model = Site
+        exclude = ['profile']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
