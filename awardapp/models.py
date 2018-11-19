b173dfa23869d5461903c9b2aff6399285151ca6
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-19 08:29:23 +0300

    made models

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,14 +1,45 @@
 from django.db import models
+from django.core.validators import URLValidator
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
-class Site(models.Model):
-    screenshot = models.ImageField(upload_to = 'images/', blank = True)
-    site_name = models.CharField(max_length = 30)
-    description = HTMLField(max_length = 70)
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = 'projects/')
+    description = models.TextField(max_length = 500)
+    link = models.TextField(validators=[URLValidator()],null=True)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
 
-    ef save_site(self):
+    def save_project(self):
         self.save()
 
-    def delete_site(self):
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_all(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_project(cls, project_id):
+        project = cls.objects.get(id=project_id)
+        return project
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects_title = cls.objects.filter(title__icontains=search_term)
+        return projects_title
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to = 'profile/')
+    profile = models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    bio = models.TextField(max_length = 100)
+    contact = models.IntegerField()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
         self.delete()
