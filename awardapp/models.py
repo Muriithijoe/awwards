d4494495d8e6606467c69e3fb553b92a1d2736b1
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-19 08:07:36 +0300

    did stuff

--- awardapp/models.py
+++ awardapp/models.py
@@ -6,3 +6,9 @@ class Site(models.Model):
     screenshot = models.ImageField(upload_to = 'images/', blank = True)
     site_name = models.CharField(max_length = 30)
     description = HTMLField(max_length = 70)
+
+    ef save_site(self):
+        self.save()
+
+    def delete_site(self):
+        self.delete()
