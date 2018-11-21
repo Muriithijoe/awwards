5f9be247506d29a3a11102369e3800b99598398e
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-21 09:48:36 +0300

    made urls for serializers

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -11,7 +11,8 @@ urlpatterns=[
     # url(r'^create-profile/$',views.create_profile,name = 'create-profile'),
     # url(r'^search/', views.search_results, name='search_results'),
     # url(r'^site-detail/(\d+)',views.search_site,name = 'site-detail'),
-
+    url(r'^api/profile/$', views.ProfileList.as_view())
+    url(r'^api/project/$', views.ProjectList.as_view())
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
