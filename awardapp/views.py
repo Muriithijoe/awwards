aea1adc244148af0403a333421de1aee5acc57a9
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-22 07:47:03 +0300

    views for post of API

--- awardapp/views.py
+++ awardapp/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  Site,Profile,AwardsProfiles,AwardsProjects
 from .serializer import ProfileSerializer,ProjectSerializer
+from rest_framework import status
 
 # Create your views here.
 # @login_required(login_url='/accounts/login/')
@@ -110,8 +111,22 @@ class ProfileList(APIView):
         serializers = ProfileSerializer(all_profile, many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+    serializers = ProfileSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class ProjectList(APIView):
     def get(self, request, format=None):
         all_project = AwardsProjects.objects.all()
         serializers = ProjectSerializer(all_project, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+    serializers =  ProjectSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
